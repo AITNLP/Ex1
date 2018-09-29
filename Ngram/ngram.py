a3e0122df738c258f4d468ed0e4d2d3aba3d6fcc
Sri Ram <k.sriramsagar@gmail.com>
2018-09-29 15:54:09 -0400

    Initial commit for ngram

---
+++ Ngram/ngram.py
@@ -0,0 +1,11 @@
+'''
+@Description:
+@Authors: Sri Ram Sagar Kappagantula
+          Harsimrat Kaur
+          Ritika De
+@Date:
+'''
+import sys
+if __name__ == "__main__":
+    ngram = sys.argv[1]
+    files = sys.argv[2:]
